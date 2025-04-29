262cb5dae31105e33c42dda6834a203d4f4e9e02
Ben Ricket <ben.ricket@gmail.com>
2025-04-29 16:10:06 -0400

    Change candidate parameter values in batch simulation

--- fumes/examples/epr_examples/ybw_phumes_phortex_batch.py
+++ fumes/examples/epr_examples/ybw_phumes_phortex_batch.py
@@ -1,28 +1,39 @@
 """ Script to iterate over multiple options of the ybw_phumes_phortex example """
 from fumes.examples.epr_examples import ybw_phumes_phortex
 
-heights = [10, 20, 50, 100]
-lengths = [10, 25, 70, 200]
-orders = [[1,1,0,0,0],[1,1,1,0,0],[0,0,0,0,0]]
-rect_scaling = [0.01, 0.1, 0.5, 1]
-secondary_scaling = [1, 5, 10]
-reward = "SampleValuesPrioritizeMid"
-for i in range(4):
-    for order in orders:
-        for rect_scale in rect_scaling:
+rewards = ["SampleValues","SampleValuesPrioritizeMid"]
+count = 0
+failed = 0
+#order_scale_groups = [([0,0,0,0,0],[1,1,1,1,1]),([0,0,0,0,0],[0.5,0.5,1,1,1]),
+#                      ([0,0,0,0,0],[0.1,0.1,1,1,1]),([1,1,1,0,0],[1,1,1,1,1]),
+#                      ([1,1,1,0,0],[0.5,0.5,1,1,1])]
+
+candidate_params = [[50,100,1,[0,0,0,0,0]],[25,50,1,[0,0,0,0,0]],
+                    [10,10,1,[0,0,0,0,0]],[10,100,1,[1,1,1,0,0]],
+                    [50,100,1,[1,1,1,0,0]],[10,50,0.5,[1,1,1,0,0]],
+                    [50,10,0.5,[1,1,1,0,0]],[50,50,0.5,[0,0,0,0,0]],
+                    [50,10,0.5,[0,0,0,0,0]],[10,50,0.1,[0,0,0,0,0]]]
+
+for i in range(5):
+    for reward in rewards:
+        for group in candidate_params:
             try:
                 params = {
-                    "height":heights[i],
-                    "length":lengths[i],
-                    "order":order,
-                    "scaling":[rect_scale,rect_scale,1,1,1],
+                    "height":group[0],
+                    "length":group[1],
+                    "order":group[3],
+                    "scaling":[group[2],group[2],1,1,1],
                     "adjust_secondary_params":[1,1,1,1,1],
-                    "reward_function":"SampleValuesPrioritizeMid"
+                    "reward_function":"SampleValues",
+                    "date":"0429",
+                    "sim_num":i
                 }
                 ybw_phumes_phortex.main_with_params(params)
-                print("Ran sim!")
+                count += 1
+                print(f"Ran sim! Count: {count}, failed: {failed}, cycle {i}")
             except Exception:
                 print(f"\n\n\nFailed for params = {params}")
+                failed += 1
 
 
     
